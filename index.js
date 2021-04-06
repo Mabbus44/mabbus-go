1dcf3dee94e425de162d6e0b2bf3064991f61a7c
Mabbus44 <rasun44@hotmail.com>
2021-04-06 21:01:11 +0200

    File creation

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("HELLO WORLD");
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const { Pool } = require("pg");
+
+const server = express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/login"))
+  .get("/login", (req, res) => res.render("pages/login"))
+  .get("/main", (req, res) => res.render("pages/main"))
+  .get("/play", (req, res) => res.render("pages/play"))
+  .get("/profile", (req, res) => res.render("pages/profile"))
+  .get("/register", (req, res) => res.render("pages/register"))
+  .get("/replay", (req, res) => res.render("pages/replay"))
+  .get("/start-new-game", (req, res) => res.render("pages/start-new-game"))
+  .get("/head", (req, res) => res.render("partials/head"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
